90c472e96d438c2fe1bc6887c74a509b9f48d1ae
Cauã Olivio <caua.olivio@gmail.com>
2026-09-01 20:32:19 -0300

    feat: implementa placar com ViewModel com SavedStateHandle

--- app/src/main/java/br/edu/ifsp/scl/sc3039587/pingpongscoreboard/PingPongViewModel.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3039587/pingpongscoreboard/PingPongViewModel.kt
@@ -1,27 +1,33 @@
 package br.edu.ifsp.scl.sc3039587.pingpongscoreboard
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PingPongViewModel : ViewModel() {
-    private val _scoreA = MutableStateFlow(0)
+class PingPongViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    private val _scoreA = MutableStateFlow(savedStateHandle["scoreA"] ?: 0)
     val scoreA: StateFlow<Int> = _scoreA.asStateFlow()
 
-    private val _scoreB = MutableStateFlow(0)
+    private val _scoreB = MutableStateFlow(savedStateHandle["scoreB"] ?: 0)
     val scoreB: StateFlow<Int> = _scoreB.asStateFlow()
 
     fun incrementScoreA() {
         _scoreA.value++
+        savedStateHandle["scoreA"] = _scoreA.value
     }
 
     fun incrementScoreB() {
         _scoreB.value++
+        savedStateHandle["scoreB"] = _scoreB.value
     }
 
     fun resetMatch() {
         _scoreA.value = 0
         _scoreB.value = 0
+        savedStateHandle["scoreA"] = 0
+        savedStateHandle["scoreB"] = 0
     }
 }
